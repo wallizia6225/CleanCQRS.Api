0509491b3ba707e19f0d8344e52d25dc1348f5cd
Waleed <walizia200@gmail.com>
2024-12-26 01:53:04 +0500

    Options pattern accessibility over layers

--- CleanCQRS.Api/Controllers/EmployeeController.cs
+++ CleanCQRS.Api/Controllers/EmployeeController.cs
@@ -1,15 +1,17 @@
 ﻿using CleanCQRS.Application.Command;
 using CleanCQRS.Application.Query;
 using CleanCQRS.Core.Entities;
+using CleanCQRS.Core.Options;
 using MediatR;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 
 namespace CleanCQRS.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class EmployeeController(IMediator mediator) : ControllerBase
+    //#OPTIONSPATTERN 6 (2nd parameter)
+    public class EmployeeController(IMediator mediator, IOptions<ConnectionStringsOptions> options) : ControllerBase
     {
         [HttpPost]
         public async Task<IActionResult> AddEmployee(EmployeeEntity employeeEntity)
@@ -21,6 +23,8 @@ namespace CleanCQRS.Api.Controllers
         [HttpGet]
         public async Task<IActionResult> GetAllEmployees()
         {
+            //#OPTIONSPATTERN 7 check value
+            var checkConString = options.Value.DefaultConnection;
             var result = await mediator.Send(new GetAllEmployeesQuery());
             return Ok(result);
         }
