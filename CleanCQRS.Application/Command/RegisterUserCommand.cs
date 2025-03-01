c84c19d355c7a59fe252209838c93449e907fbac
Waleed <walizia200@gmail.com>
2025-03-01 05:47:24 +0500

    Add user registeration

---
+++ CleanCQRS.Application/Command/RegisterUserCommand.cs
@@ -0,0 +1,23 @@
+﻿using CleanCQRS.Core.DTO.UserManagment;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanCQRS.Application.Command
+{
+    public record RegisterUserCommand(UserRegisterDto UserRegister) : IRequest<UserRegisterDto>;
+
+    //    public class AddEmployeeCommandHandler(IEmployeeRepository employeeRepository, IMediator mediator) : IRequestHandler<AddEmployeeCommand, EmployeeEntity>
+
+    public class RegiaterUserCommandHandler(IMediator mediator) : IRequestHandler<RegisterUserCommand, UserRegisterDto>
+    {
+        public Task<UserRegisterDto> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
---
+++ CleanCQRS.Core/DTO/UserManagment/UserRegisterDto.cs
@@ -0,0 +1,8 @@
+﻿namespace CleanCQRS.Core.DTO.UserManagment
+{
+    public class UserRegisterDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
